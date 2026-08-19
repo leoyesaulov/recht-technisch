e6f58629d1f851b92bf6794727b8025c16bc9703
Leo Yesaulov <leo@omniserv.me>
2026-08-19 10:30:27 +0200

    add basic frontend

--- api/main.py
+++ api/main.py
@@ -6,4 +6,8 @@ app = FastAPI()
 def main():
     return {"name": "Legal Loves Tech 2026", "message": "Hello World!"}
 
+@app.get("/test_connection")
+def test_connection():
+    return {"password": "abhschda"}
+
 uvicorn.run(app, host="0.0.0.0", port=8080)
